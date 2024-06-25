4d85a2ad430ec3f2ac7ce4e6c58e3597eb70d354
Suman Mondal <mondalsumanrock20@gmail.com>
2024-06-25 15:45:29 +0530

    vercel font added

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { cn } from "@/lib/utils";
+import { GeistSans } from "geist/font/sans";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -15,8 +17,10 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html lang="en" suppressHydrationWarning>
+      <body className={cn(GeistSans.variable, "font-sans")}>
+        {children}
+      </body>
     </html>
   );
 }
