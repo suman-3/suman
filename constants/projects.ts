f38dbbb5653a0012eaf8890571042f3f9ddd1da6
Suman Mondal <mondalsumanrock20@gmail.com>
2024-07-06 00:33:28 +0530

    project 2 added

--- constants/projects.ts
+++ constants/projects.ts
@@ -1,9 +1,16 @@
 export const projects = [
     {
         title: "Seed",
-        description: "Seed is a web app that enables users to design software architectures with ease...",
+        description: "A open source design tool that enables users to design software architectures with ease...",
         github: "https://github.com/suman-3/seed",
         image: "/projects/project1.png",
-        link: "https://seed.exions.xyz/",
+        link: "https://seed.exions.xyz",
+    },
+    {
+        title: "Praxis",
+        description: "An AI-driven app that simulates realistic job interviews and provides feedback to help users improve their performance.",
+        github: "https://github.com/suman-3/praxis",
+        image: "/projects/project2.png",
+        link: "https://praxis.exions.xyz",
     },
 ];
